a56dee41fc154587c52bc988435680f65b7254e5
John Koehler <john.w.koehler@gmx.de>
2017-11-22 15:07:26 +0100

    Adding Static Pages to Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#index'
+
+  get '/about', to: 'static_pages#about'
+
+  get '/help', to: 'static_pages#help'
+
+  get '/contact', to: 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
