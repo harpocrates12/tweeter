611f87672929024850a0d04648ba254099c11993
John Koehler <john.w.koehler@gmx.de>
2017-11-23 11:50:13 +0100

    Finish Basic login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  root                   'static_pages#index'
 
-  root 'static_pages#index'
+  get    '/about',   to: 'static_pages#about'
 
-  get '/about', to: 'static_pages#about'
+  get    '/help',    to: 'static_pages#help'
 
-  get '/help', to: 'static_pages#help'
+  get    '/contact', to: 'static_pages#contact'
 
-  get '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
 
-  get '/signup', to: 'users#new'
+  post   '/signup',  to: 'users#create'
 
-  post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+
+  post   '/login',   to: 'sessions#create'
+
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
